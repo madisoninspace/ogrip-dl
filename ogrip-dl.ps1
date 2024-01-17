14a17071024eeb1260aefd015093f22c368cfde0
Madison Wass <931615+madisoninspace@users.noreply.github.com>
2024-01-16 22:04:50 -0500

    Corrected download problems

--- ogrip-dl.ps1
+++ ogrip-dl.ps1
@@ -13,7 +13,7 @@ if (!(Test-Path $tsvFilePath)) {
 
 # Parse TSV File
 $ids = Get-Content -Path $tsvFilePath
-$ids = $ids | ForEach-Object { $_.Split("`t")[0] }
+#$ids = $ids | ForEach-Object { $_.Split("`t")[0] }
 
 # Check if the lidar directory exists. If not, create it. 
 # If it already exists, empty it.
@@ -27,21 +27,20 @@ if (!(Test-Path -Path "lidar")) {
 
 # Ask the user for the county code. These are three letters.
 # If the code is Columbus, allow it.
-$countyCode = Read-Host -Prompt "Please enter the county code:"
-if ($countyCode == "Columbus" || $countyCode -match '^\w{3}$') {
-    foreach ($id in $ids) {
-        Invoke-WebRequest -Uri "$baseUrl/$countyCode/$id.zip" -OutFile "$id.zip"
+foreach ($id in $ids) {
+    $lidarId = $id.Split("`t")[0]
+    $countyId = $id.Split("`t")[1]
+    $fileUrl = "$baseUrl$countyId/$lidarId.zip"
+
+    Invoke-WebRequest -URI $fileUrl -OutFile "$lidarDirectory\$lidarId.zip"
+}
+
+if ((Get-ChildItem -Path "$lidarDirectory").Count -gt 0) {
+    $zipFiles = Get-ChildItem -Path "$lidarDirectory\*.zip"
+    foreach ($zipFile in $zipFiles) {
+        Expand-Archive -Path $zipFile.FullName -DestinationPath "$lidarDirectory" -Force
     }
 
-    if ((Get-ChildItem -Path "$lidarDirectory").Count -gt 0) {
-        $zipFiles = Get-ChildItem -Path "$lidarDirectory\*.zip"
-        foreach ($zipFile in $zipFiles) {
-            Expand-Archive -Path $zipFile.FullName -DestinationPath "$lidarDirectory" -Force
-        }
-
-        Remove-Item -Path "lidar\*.zip" -Force
-    }
-} else {
-    Write-Host "Invalid county code."
-    Exit 1
+    Remove-Item -Path "$lidarDirectory\*.zip" -Force
+    Remove-Item -Path "$lidarDirectory\*.xml" -Force
 }
